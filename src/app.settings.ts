f5200efeedcec1688a53c474a4bf95d8b4c8fb89
Arthur Fritz Santiago <arthur.santiago@matera.com>
2017-08-22 09:29:10 -0300

    Redirecionando para outro servidor cloud

--- src/app.settings.ts
+++ src/app.settings.ts
@@ -1,4 +1,4 @@
 export class AppSettings {
-   public static API_ENDPOINT='http://localhost:8000/api/v1/';
-   public static API_OAUTH='http://educamais.herokuapp.com/oauth/token';
+   public static API_ENDPOINT='http://educa-educamais.a3c1.starter-us-west-1.openshiftapps.com/api/v1/';
+   public static API_OAUTH='http://educa-educamais.a3c1.starter-us-west-1.openshiftapps.com/oauth/token';
 }
